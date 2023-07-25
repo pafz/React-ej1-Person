591388690db129faedbb9400597a71d2e7c2f009
pafz <paferza@gmail.com>
2023-07-25 17:26:40 +0200

    PersonClass edited without comas

--- src/components/PersonClass/PersonClass.jsx
+++ src/components/PersonClass/PersonClass.jsx
@@ -4,7 +4,7 @@ class PersonClass extends Component {
     render() {
       console.log(this.props.name)
     return (
-      <span>{this.props.name}, {this.props.surname}, {this.props.age}</span>
+      <span>{this.props.name} {this.props.surname} {this.props.age}</span>
     )
   }
 }
