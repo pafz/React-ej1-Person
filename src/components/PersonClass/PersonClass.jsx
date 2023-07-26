37a9d2595a97f5224a805203d397a4dc57ef89e5
pafz <paferza@gmail.com>
2023-07-26 09:16:06 +0200

    better code after corrections

--- src/components/PersonClass/PersonClass.jsx
+++ src/components/PersonClass/PersonClass.jsx
@@ -3,8 +3,10 @@ import { Component } from 'react'
 class PersonClass extends Component {
     render() {
       console.log(this.props.name)
+      //const {name, surname, age} = this.props; //BETTER, MORE CLEAN
     return (
       <span>{this.props.name} {this.props.surname} {this.props.age}</span>
+      //<span>{name} {surname} {age}</span> //BETTER, MORE CLEAN
     )
   }
 }
